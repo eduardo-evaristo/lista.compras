f1ce28b4fbc90bc6891375ba3ccb81163fbfcffe
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2024-08-11 06:24:58 -0300

    New components

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,65 @@ import { unitData, optionsData } from "./Data.jsx";
 
 export default function App() {
   return (
-    <Form>
-      <Input>Nome:</Input>
-      <Input>Quantidade:</Input>
-      <Select data={unitData}>Unidade:</Select>
-      <Select data={optionsData}>Estado:</Select>
-      <Button>Enviar</Button>
-    </Form>
+    <>
+      <Header />
+      <HeroSection />
+      <Form>
+        <Input>Nome:</Input>
+        <Input>Quantidade:</Input>
+        <Select data={unitData}>Unidade:</Select>
+        <Select data={optionsData}>Estado:</Select>
+        <Button>Enviar</Button>
+      </Form>
+    </>
   );
 }
 
 function Button({ children }) {
   return <button>{children}</button>;
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__logo">Hello</h1>
+      <Navbar>
+        <NavbarItem>Teste 1</NavbarItem>
+        <NavbarItem>Teste 2</NavbarItem>
+        <NavbarItem>Teste 3</NavbarItem>
+      </Navbar>
+    </header>
+  );
+}
+
+function Navbar({ children }) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__list">{children}</ul>
+    </nav>
+  );
+}
+
+function NavbarItem({ children }) {
+  return (
+    <li className="navbar__list__item">
+      <button className="navbar__list__item__button">{children}</button>
+    </li>
+  );
+}
+
+function HeroSection() {
+  return (
+    <div className="hero__section">
+      <div className="hero__section__container">
+        <div className="hero__section__container__text">
+          <h1 className="hero__section__container__text__title">
+            Tenha mais controle sob suas compras!
+          </h1>
+        </div>
+        <button className="hero__section__container__button">Começar</button>
+      </div>
+      <img src="assets/heropic.jpg" alt="" className="hero__section__image" />
+    </div>
+  );
 }
